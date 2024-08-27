d89a2a9b8f9d2f660440485c8e53dab4b5efe2e4
Matheus Teixeira <matheustxr.dev@gmail.com>
2024-08-27 19:07:25 -0300

    created seed bd

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -1,6 +1,9 @@
+import { hash } from 'bcryptjs'
 import type { FastifyInstance } from 'fastify'
 import type { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod'
+
+import { prisma } from '@/lib/prisma'
 
 export async function createAccount(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
@@ -14,8 +17,30 @@ export async function createAccount(app: FastifyInstance) {
         }),
       },
     },
-    () => {
-      return 'User created!'
+    async (request, response) => {
+      const { name, email, password } = request.body
+
+      const userWithEmail = await prisma.user.findUnique({
+        where: { email },
+      })
+
+      if (userWithEmail) {
+        return response
+          .status(400)
+          .send({ message: 'User with same e-mail already exists.' })
+      }
+
+      const passwordHash = await hash(password, 6)
+
+      await prisma.user.create({
+        data: {
+          name,
+          email,
+          passwordHash,
+        },
+      })
+
+      return response.status(201).send()
     },
   )
 }
