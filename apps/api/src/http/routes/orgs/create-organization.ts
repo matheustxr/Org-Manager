3f3c7f868992fd5a889e7bfad0428247c681246c
Matheus Teixeira <matheustxr.dev@gmail.com>
2024-08-31 19:48:20 -0300

    create route create-organization

---
+++ apps/api/src/http/routes/orgs/create-organization.ts
@@ -0,0 +1,70 @@
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+import { createSlug } from '@/utils.ts/create-slug'
+
+import { BadRquestError } from '../_errors/bad-request-error'
+
+export async function createOrganization(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .post(
+      '/organization',
+      {
+        schema: {
+          tags: ['organizations'],
+          summary: 'Create a new organization',
+          security: [{ bearerAuth: [] }],
+          body: z.object({
+            name: z.string(),
+            domain: z.string().nullish(),
+            shouldAttachUsersByDomain: z.boolean().optional(),
+          }),
+          response: {
+            201: z.object({
+              organizationId: z.string().uuid(),
+            }),
+          },
+        },
+      },
+      async (request, response) => {
+        const userId = await request.getCurrentUserId()
+        const { name, domain, shouldAttachUsersByDomain } = request.body
+        if (domain) {
+          const organizationByDomain = await prisma.organization.findUnique({
+            where: { domain },
+          })
+
+          if (organizationByDomain) {
+            throw new BadRquestError(
+              'Another organization with the same domain already exists',
+            )
+          }
+        }
+
+        const organization = await prisma.organization.create({
+          data: {
+            name,
+            slug: createSlug(name),
+            domain,
+            shouldAttachUsersByDomain,
+            ownerId: userId,
+            members: {
+              create: {
+                userId,
+                role: 'ADMIN',
+              },
+            },
+          },
+        })
+
+        return response.status(201).send({
+          organizationId: organization.id,
+        })
+      },
+    )
+}
