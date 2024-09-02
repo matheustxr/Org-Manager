b2d4907b6806af6aa0f9ccd3d10e6c8b1f798208
Matheus Teixeira <matheustxr.dev@gmail.com>
2024-09-02 01:17:48 -0300

    created routes update and remove member

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -19,6 +19,8 @@ import { getProfile } from './routes/auth/get-profile'
 import { requestPasswordRecover } from './routes/auth/request-password-recover'
 import { resetPassword } from './routes/auth/reset-password'
 import { getMembers } from './routes/members/get-members'
+import { removeMember } from './routes/members/remove-member'
+import { updateMember } from './routes/members/update-member'
 import { createOrganization } from './routes/orgs/create-organization'
 import { deleteOrganization } from './routes/orgs/delete-organization'
 import { getMembership } from './routes/orgs/get-membership'
@@ -91,7 +93,8 @@ app.register(getProjects)
 app.register(updateProject)
 
 app.register(getMembers)
-
+app.register(updateMember)
+app.register(removeMember)
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('HTTP server running!')
 })
