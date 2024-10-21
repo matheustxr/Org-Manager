c14fb3a90795668a01d1d3472d67ec09139b36cb
Matheus Teixeira <matheustxr.dev@gmail.com>
2024-10-21 19:52:32 -0300

    changed environment variables

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -114,6 +114,6 @@ app.register(getPendingInvites)
 
 app.register(getOrganizationBilling)
 
-app.listen({ port: env.SERVER_PORT }).then(() => {
-  console.log(`HTTP server running at port ${env.SERVER_PORT}!`)
+app.listen({ port: env.PORT }).then(() => {
+  console.log(`HTTP server running at port ${env.PORT}!`)
 })
